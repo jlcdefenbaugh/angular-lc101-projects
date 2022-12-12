d72ab1194ac5c4cb950c16471cee605cca65e426
Jami <jamidefenbaugh@gmail.com>
2022-12-11 22:04:24 -0600

    completed tasks in part 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Kick-Ass Photos';
+  image1 = 'https://th.bing.com/th/id/OIP.l0hQ3Daq6X19WoF2qy1MigHaJ4?w=198&h=264&c=7&r=0&o=5&pid=1.7';
+  image2 = 'https://th.bing.com/th/id/OIP.tlymlo9ZmZL0L0OBOscegQHaLL?w=198&h=299&c=7&r=0&o=5&pid=1.7';
+  image3 = 'https://th.bing.com/th/id/OIP.0KoCfDizXzGsHBUyd-ptZQHaHa?w=198&h=198&c=7&r=0&o=5&pid=1.7';
 
   constructor() { }
 
