c634fa409a0c1a3d8595946e866ab040a55ea00e
Jami <jamidefenbaugh@gmail.com>
2022-12-13 10:49:01 -0600

    updated HTML

--- lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
+++ lesson2/examples/ngif-practice/src/app/chores/chores.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
    styleUrls: ['./chores.component.css']
 })
 export class ChoresComponent implements OnInit {
-   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries'];
-   finishedChores = [];
+   chores = ['Empty dishwasher', 'Complete LaunchCode prep work', 'Buy groceries', 'Sit the fuck down'];
+   finishedChores = ['Sitting the fuck down'];
 
-   targetImage = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
+   targetImage = 'https://www.bing.com/th?id=OIP.ZCHKvtzpk_w_lq-kmSpMtgHaDt&w=350&h=175&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2';
 
    constructor() { }
 
