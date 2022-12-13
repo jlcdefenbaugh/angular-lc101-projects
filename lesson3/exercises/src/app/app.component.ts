652038e1d13d27ddc2a716965c619be6aefa8e3c
Jami <jamidefenbaugh@gmail.com>
2022-12-13 14:33:42 -0600

    completed all tasks. Relied on solutions branch. Sigh.

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,6 +12,38 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled = true;
+
+
+  gutterCheck(width, height) {
+    if (width > 260000 || width < 0 || height < 0 || height > 340000) {
+      this.color = 'orange';
+    } else {
+      this.color = 'blue';
+    }
+  }
+
+
+  moveRocket(rocketImage, direction) {
+    if (direction === 'right') {
+      let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+    } else if (direction === 'left') {
+      let movement = parseInt(rocketImage.style.left) - 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width= this.width - 10000;
+    } else if (direction === 'down') {
+      let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height = this.height - 10000;
+    } else {
+      let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height = this.height + 10000;
+    }
+    this.gutterCheck(this.width, this.height);
+  }
 
 handleTakeOff() {
   let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
@@ -20,6 +52,7 @@ handleTakeOff() {
     this.height = 10000;
     this.width = 0;
     this.message = 'Shuttle in flight.';
+    this.takeOffEnabled = false;
   };
 };
 
@@ -29,6 +62,9 @@ handleLand(rocketImage) {
     this.height = 0;
     this.width = 0;
     this.message = 'The shuttle has landed.';
+    this.takeOffEnabled = true;
+    rocketImage.style.bottom = '0px';
+    rocketImage.style.left = '0px';
 };
 
 handleAbort(rocketImage) {
@@ -38,14 +74,9 @@ handleAbort(rocketImage) {
     this.height = 0;
     this.width = 0;
     this.message = 'Mission aborted.';
-  };
-};
-
-moveRocket(rocketImage, direction) {
-  if (direction === 'right') {
-    let movement = parseInt(rocketImage.style.left) + 10 + 'px';
-    rocketImage.style.left = movement;
-    this.width = this.width + 10000;
+    this.takeOffEnabled = true;
+    rocketImage.style.bottom = '0px';
+    rocketImage.style.left = '0px';
   }
 }
 
